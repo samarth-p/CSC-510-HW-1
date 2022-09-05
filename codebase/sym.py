387d2251e5ab1a5e80112485382388c4e6a03c91
Tejas Prabhu <tejas.prabhu29@gmail.com>
2022-09-05 19:01:57 -0400

    Create sym class and add sym related methods

---
+++ codebase/sym.py
@@ -0,0 +1,30 @@
+import math
+from collections import defaultdict
+
+class Sym():
+
+    def __init__(self, c ,s):
+        self.n = 0
+        self.at = c or True
+        self.name = s or True
+        self._has = defaultdict(lambda: 0)
+    
+    def add(self, v):
+        if v != '?':
+            self.n = self.n + 1
+        self._has[v] = 1 + self._has[v]
+
+    def div(self):
+        e = 0
+        for item in self._has.values():
+            if item > 0:
+                e = e - (item/self.n)*math.log((item/self.n),2)
+        return e
+    
+    def mid(self):
+        most = -1
+        for k,v in self._has.items():
+            if v > most:
+                mode, most = k, v
+        return mode
+
