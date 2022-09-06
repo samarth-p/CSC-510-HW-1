dbb7c32d103adebc6b25b0362084923121cbc119
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-09-06 09:57:34 -0400

    Update test_sym.py
    modified the sym class wrt cli

--- test/test_sym.py
+++ test/test_sym.py
@@ -1,4 +1,5 @@
 from codebase.sym import Sym
+from codebase.cli import the
 
 
 def test_sym():
@@ -8,10 +9,11 @@ def test_sym():
         sym.add(element)
     mode, entropy = sym.mid(), sym.div()
     entropy = (1000*entropy)//1/1000
-    print(mode, entropy)
+    print("Mode : {}" .format(mode))
+    print("Entropy : {}" .format(entropy))
     assert mode == 'a' and 1.37 <= entropy <= 1.38
 
 
-# def test_the():
-#     print(the)
-#     return true
+def test_the():
+    print("List of config parameters : {}" .format(the))
+    assert True
