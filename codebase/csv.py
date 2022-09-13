9ee34443721cf76a662335a78bbaa1f8eaa0cee8
Kartik Rawool <kartikr2k133@gmail.com>
2022-09-12 22:12:54 -0400

    create csv method update cli

---
+++ codebase/csv.py
@@ -0,0 +1,32 @@
+from cli import the
+from cli import coerce
+import os
+
+def csv(fname, func):
+    script_path = os.path.abspath(__file__) 
+    script_dir = os.path.split(script_path)[0] 
+    rel_path = fname
+    abs_file_path = os.path.join(script_dir, rel_path)
+    sep = the["Seperator"]
+    with open(abs_file_path, 'r') as f:
+        t = []
+        for line in f:
+            words = line.rstrip('\n').split(sep)
+            words_list = []
+            for word in words:
+                w = coerce(word)
+                words_list.append(w)
+            t.append(words_list[0:])
+        
+        func(t)
+
+
+
+
+def test_csv():
+    def fun(row):
+        print(row[:10])# oo(row) yet to implement
+    csv("../data/auto93.csv", fun)
+    return True
+
+test_csv()
