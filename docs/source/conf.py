3324ef6c53dca769d373d3f3af706fea707e7c99
Kartik Rawool <55804665+kartikrawool@users.noreply.github.com>
2022-10-02 00:45:21 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 project = 'Numerical Analysis(Lua to Python)'
